5f615179cc70e48cf482f123913af6e658a6b131
Sachin Tiwari <sachin.tiwari@uneecops.in>
2025-09-16 11:22:48 +0530

    new demo

---
+++ demoproject/target/debug/deps/demoproject-96df899dddc781df.d
@@ -0,0 +1,5 @@
+C:\Users\sachin.tiwari\Clonerust\NewProject\demoSachin\demoproject\target\debug\deps\demoproject-96df899dddc781df.d: src\main.rs
+
+C:\Users\sachin.tiwari\Clonerust\NewProject\demoSachin\demoproject\target\debug\deps\libdemoproject-96df899dddc781df.rmeta: src\main.rs
+
+src\main.rs:
---
+++ demoproject/target/debug/deps/demoproject-eccbaafd2237ea8f.d
@@ -0,0 +1,5 @@
+C:\Users\sachin.tiwari\Clonerust\NewProject\demoSachin\demoproject\target\debug\deps\demoproject-eccbaafd2237ea8f.d: src\main.rs
+
+C:\Users\sachin.tiwari\Clonerust\NewProject\demoSachin\demoproject\target\debug\deps\libdemoproject-eccbaafd2237ea8f.rmeta: src\main.rs
+
+src\main.rs:
